e77c56a27b0c983e2b468e1868b0b5105f52c748
SAURALINI <saurabhkiller01@gmail.com>
2024-05-17 11:39:19 +0530

    first commit

---
+++ conditon_loops.java
@@ -0,0 +1,52 @@
+package test;
+
+public class conditon_loops {
+	public static void main(String[]args) {
+		
+		int a; // declaration that some value will be saved in the variable a
+		int b=5; // value initialise 
+		
+		int c=20;
+		String s="java";
+		
+		System.out.println(s);
+		System.out.println(b);
+		System.out.println(b+c);
+		System.out.println(s+b+c);// alphabet + numeric with get concatenate
+		
+		// condition below // if -else condition
+		a=3;
+		
+		if(a>b) {
+			System.out.println("a is greater");
+		}else {
+			System.out.println("b is greater");
+		}
+		// if else with AND OR operator
+		
+		if(a>b & a>c) {
+			System.out.println("a is greater");
+		}
+		else if (b>c) {
+			System.out.println("b is greater");
+		}
+		else {
+			System.out.println("c is greater");
+			
+		}
+		
+		
+		//loops to study
+		//i=0// initilization
+		//i>5, contidion
+		//i++ increement operator
+		// for loop 
+		System.out.println();
+		for (int i=0;i<5;i++)
+		{
+			System.out.println("java  "+i);
+		}
+		
+	}
+
+}
---
+++ understanding_java.java
@@ -0,0 +1,24 @@
+package test;
+
+public class understanding_java	{
+
+	public static void main(String[] args) {
+		
+		int a=100; //integer data type will store only 32bits
+		long b=1452662262; // long used to store 64bit data
+		float c=25622.00f; // store 2 decimal data
+		double d= 10245.0023d; // store more than 2 decimal data
+		
+		char e='s';  // store only one character
+		String f="saurabh is learning java"; // to store a line para and more
+		boolean g=true; // to print boolean values
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+}
